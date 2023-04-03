713125242fa4144954e806d67a0a7a749f4de7e8
xiaomao <1216243405@qq.com>
2023-04-03 17:43:10 +0800

    fix:使用重定向

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,18 @@ import { StepBackwardOutlined } from "@ant-design/icons";
 import "../src/assets/styles/global.scss";
 
 import ButtonCom from "./components/ButtonCom";
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       后台管理系统练习
-      <ButtonCom />
-      <StepBackwardOutlined style={{ fontSize: "28px" }} />
+      <hr />
+      {/* <ButtonCom />
+      <StepBackwardOutlined style={{ fontSize: "28px" }} /> */}
       {/* 占位符组件，类似于窗口，用来展示组件的 */}
+      <Link to={'/home'}>Home</Link>
+      <Link to={'/about'}>About</Link>
       <Outlet></Outlet>
     </div>
   );
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,15 @@
 import App from "../App";
 import Home from "../views/Home";
 import About from "../views/About";
-import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
 const baseRouter = () => {
     return <BrowserRouter>
-    <Routes>
+    <Routes>    
         <Route path="/" element={<App />}>
+            {/* 配置重定向 */}
+            <Route path="/" element={<Navigate to={'/home'} />}>
+            </Route>
             <Route path="/home" element={<Home />} ></Route>
             <Route path="/about" element={<About />} ></Route>
         </Route>
