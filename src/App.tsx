853fb31ffd00a170356f5e5e105a71f1ff9effd3
xiaomao <1216243405@qq.com>
2023-04-03 17:23:46 +0800

    fix:使用route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import "reset-css";
+import { StepBackwardOutlined } from "@ant-design/icons";
 
-import '../src/assets/styles/global.scss';
+import "../src/assets/styles/global.scss";
+
+import ButtonCom from "./components/ButtonCom";
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App">后台管理系统练习</div>;
+  return (
+    <div className="App">
+      后台管理系统练习
+      <ButtonCom />
+      <StepBackwardOutlined style={{ fontSize: "28px" }} />
+      {/* 占位符组件，类似于窗口，用来展示组件的 */}
+      <Outlet></Outlet>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ButtonCom.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+
+const ButtonCom = () => {
+    return (
+        <div className='button-wrap'>
+             <Button type="primary">Primary Button</Button>
+        </div>
+    );
+};
+
+export default ButtonCom;
